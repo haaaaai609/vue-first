b0c235dc0c009efd26d7e4e9c9fef2842303df17
Haaaaai <1306625287@qq.com>
2024-04-20 21:13:24 +0800

    titleStyleUpdate

--- scritps/preinstall.js
+++ scritps/preinstall.js
@@ -2,7 +2,7 @@
  * @Author: Haaaaai 1306625287@qq.com
  * @Date: 2024-03-12 16:35:34
  * @LastEditors: Haaaaai 1306625287@qq.com
- * @LastEditTime: 2024-04-20 20:09:15
+ * @LastEditTime: 2024-04-20 21:11:58
  * @FilePath: \vue3_admin_template\project\scritps\preinstall.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
--- src/layout/Logo/index.vue
+++ src/layout/Logo/index.vue
@@ -2,7 +2,7 @@
  * @Author: Haaaaai 1306625287@qq.com
  * @Date: 2024-03-18 17:00:09
  * @LastEditors: Haaaaai 1306625287@qq.com
- * @LastEditTime: 2024-03-20 11:38:10
+ * @LastEditTime: 2024-04-20 21:12:49
  * @FilePath: \vue3_admin_template\project\src\layout\Logo\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -26,15 +26,18 @@ export default {
 .logo {
   display: flex;
   align-items: center;
+  justify-content: center;
   height: $base-logo-height;
   padding: 0 6px;
   box-sizing: border-box;
   width: $base-menu-width;
 }
+
 img {
   width: 40px;
   height: 40px;
 }
+
 p {
   font-size: 25px;
   color: white;
